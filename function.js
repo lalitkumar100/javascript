556ee2bc3ededf4676a9cdf5b2be521bb4a372ef
lalitkumar100 <166192372+lalitkumar100@users.noreply.github.com>
2024-10-26 22:42:54 +0530

    Create function.js

---
+++ function.js
@@ -0,0 +1,38 @@
+
+// //this call arrow function
+// let func = (num,num1) =>{//  when you use { } return must be return 
+//     return num+num1;
+//  }
+// // let func = (num,num1) => num+num1; //default return statement
+
+// // let func = (num,num1) => (num+num1;) //default return statement
+// // let func = (num,num1) =>({username:"lalit"}); //default return statement
+// console.log(func(2,4));
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// username={
+//     user:"lalit",
+//     print : function ( wife){
+//         console.log(`${this.user } and his wife ${ wife}`)    
+//     }
+// }
+
+// username.print('xyz');
+// username.user="pai";
+// username.print('nidi');
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// immdetaiy  invloking of funcion |
+(
+    function add2(nums){
+        console.log(nums);
+        
+    }) ('lalit') 
+     //      ;
+    // alert bro use  { ; } after { () } like this bcause  it end immdetaiy involed function else  error art  next function
+
+    // (function add3(nums){
+    //     console.log(nums+3);
+        
+    // }) ('lalit') 
